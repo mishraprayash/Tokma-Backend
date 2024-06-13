6fd3f350db484b89f522d496233dfdfd81d3f1df
Ashimkarrki <ashimkarki987@gmail.com>
2024-06-13 21:14:23 +0545

    guide slight modify

--- controllers/guide.js
+++ controllers/guide.js
@@ -1,67 +1,74 @@
-import mongoose from "mongoose"
-import Guide from "../models/guideModel.js"
-import bcrypt from "bcryptjs"
+import mongoose from "mongoose";
+import Guide from "../models/guideModel.js";
+import bcrypt from "bcryptjs";
 import Tourist from "../models/touristModel.js";
 
 export const register = async (req, res, next) => {
-    const { name, contactNo, gender, age, location, email, password } = req.body;
-    if (!name || !contactNo || !gender || !age || !location) {
-        return res.json({ message: "Missing informations" });
-    }
-    const user = await Guide.findOne({ email });
-    if (user) {
-        return res.json({ message: "User already exists" })
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const {
+    firstName,
+    lastName,
+    contactNo,
+    gender,
+    age,
+    location,
+    email,
+    password,
+  } = req.body;
+  if (!firstName || !lastName || !contactNo || !gender || !age || !location) {
+    return res.json({ message: "Missing informations" });
+  }
+  const user = await Guide.findOne({ email });
+  if (user) {
+    return res.json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const guide = await Guide.create({
-        name, contactNo, gender, age, location, email,
-        password: hashedPassword
-    })
-    return res.json({ message: "Register Success", guide });
-}
+  const guide = await Guide.create({
+    firstName,
+    lastName,
+    contactNo,
+    gender,
+    age,
+    location,
+    email,
+    password: hashedPassword,
+  });
+  return res.json({ message: "Register Success", guide });
+};
 
 export const login = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.json({ message: "Missing information" });
-        }
-        const guide = await Guide.findOne({ email });
-        if (!guide) {
-            return res.json({ message: "User doesnot exists" });
-        }
-        const isPasswordMatched = await guide.matchPassoword(password);
-        if (!isPasswordMatched) {
-            return res.json({ message: "User doesnot exist" });
-        }
-        // remaining to handle create session here 
-        const token = guide.createJWT();
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: false,
-            maxAge: 24 * 60 * 60 * 1000 // 1 day
-        });
-        return res.status(200).json({ message: "Login Success", token });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error })
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "Missing information" });
     }
-}
-
+    const guide = await Guide.findOne({ email });
+    if (!guide) {
+      return res.json({ message: "User doesnot exists" });
+    }
+    const isPasswordMatched = await guide.matchPassoword(password);
+    if (!isPasswordMatched) {
+      return res.json({ message: "User doesnot exist" });
+    }
+    // remaining to handle create session here
+    const token = guide.createJWT();
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+    return res.status(200).json({ message: "Login Success", token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+};
 
 export const fetchDashboardInfo = async (req, res, next) => {
-    try {
-        const guide = await Guide.findById(req.user.id)
-        
-        
-    } catch (error) {
-
-    }
-}
-
-
-
+  try {
+    const guide = await Guide.findById(req.user.id);
+  } catch (error) {}
+};
 
 // accept the hire request from the tourist
 // export const acceptOffer = async (req, res, next) => {
@@ -98,5 +105,3 @@ export const fetchDashboardInfo = async (req, res, next) => {
 //         return res.status(500).json({ error })
 //     }
 // }
-
-
--- models/guideModel.js
+++ models/guideModel.js
@@ -2,70 +2,71 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const guideSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contactNo: {
+    type: String,
+    maxLength: 10,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female", "Others"],
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isApproved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  role: {
+    type: String,
+    default: "guide",
+  },
+  hiringInfo: {
+    isHired: {
+      type: Boolean,
+      default: false,
     },
-    lastName: {
-        type: String,
-        required: true
+    hiringTourist: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Tourist",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    contactNo: {
-        type: String,
-        maxLength: 10,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true,
-        enum: ['Male', 'Female', 'Others']
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isApproved: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    role: {
-        type: String,
-        default: 'guide'
-    },
-    hiringInfo: {
-        isHired: {
-            type: Boolean,
-            default: false
-        },
-        hiringTourist: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: 'Tourist'
-        }
-    },
-    isAvailable:{
-        type:Boolean,
-    }
-})
+  },
+  isAvailable: {
+    type: Boolean,
+    default: false,
+  },
+});
 guideSchema.methods.createJWT = function () {
-    return jwt.sign(
-        { id: this._id, email: this.email, role: this.role },
-        process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_LIFETIME }
-    )
-}
+  return jwt.sign(
+    { id: this._id, email: this.email, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
-guideSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password);
-}
-const Guide = mongoose.model('Guide', guideSchema);
+guideSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const Guide = mongoose.model("Guide", guideSchema);
 export default Guide;
