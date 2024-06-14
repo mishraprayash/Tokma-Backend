2fdc4283766d7913944adcaf8148097cb26d991f
mishraprayash <mishraprayash11@gmail.com>
2024-06-14 16:34:34 +0545

    latest

--- controllers/admin.js
+++ controllers/admin.js
@@ -48,7 +48,7 @@ export const login = async (req, res, next) => {
         const token = admin.createJWT();
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
 
