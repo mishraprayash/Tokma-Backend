af4f04d870301e8a2e4d1fdd925a04d26d4b2d8f
mishraprayash <mishraprayash11@gmail.com>
2024-06-15 15:52:58 +0545

    updated auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,26 +2,22 @@ import jwt from "jsonwebtoken";
 import JsonWebTokenError from "jsonwebtoken/lib/JsonWebTokenError.js";
 
 export const isAuthenticated = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
-        console.log(authHeader)
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-           throw new UnauthenticatedError('Not authorized')
-        }
-        const token = authHeader.split(' ')[1]
-        return res.json({message:token})
-        if (!token) {
-            return res.status(403).json({ message: "Unauthorized Access" });
-        }
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decodedToken);
-        req.user = decodedToken
-        next()
-    } catch (error) {
-        // if(error instanceof JsonWebTokenError){
-        //     return res.status(403).json({message:"Unauthorized Access"})
-        // }
-        console.log(error);
-        return res.status(500).json({ error });
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(403).json({ message: "Unauthorized Access" });
     }
-}
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(403).json({ message: "Unauthorized Access" });
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    if (error instanceof JsonWebTokenError) {
+      return res.status(403).json({ message: "Unauthorized Access" });
+    }
+    return res.status(500).json({ error });
+  }
+};
