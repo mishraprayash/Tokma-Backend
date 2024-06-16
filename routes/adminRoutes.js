7d0ca958a9fba3da1085b2231b5b629e1eb383cd
mishraprayash <mishraprayash11@gmail.com>
2024-06-16 14:37:58 +0545

    updated routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import express from "express"
 const router = express.Router()
 
-import { register, login, approveGuide, rejectGuide, fetchDashboardInfo, approveHealthService,rejectHealthService,setRules } from "../controllers/admin.js"
+import { register, login, approveGuide, rejectGuide, fetchDashboardInfo, approveHealthService, rejectHealthService, setRules, approvefoodandlodge, rejectfoodandlodge } from "../controllers/admin.js"
 import { isAuthenticated } from "../middleware/auth.js"
 import { adminAuth } from "../middleware/admin-auth.js"
 import { logout } from "../lib/logout.js"
@@ -13,7 +13,9 @@ router.route('/approve-guide/:id').get(isAuthenticated, adminAuth, approveGuide)
 router.route('/reject-guide/:id').get(isAuthenticated, adminAuth, rejectGuide)
 router.route('/approve-healthservice/:id').get(isAuthenticated, adminAuth, approveHealthService)
 router.route('/reject-healthservice/:id').get(isAuthenticated, adminAuth, rejectHealthService)
-router.route('/rule').post(isAuthenticated,adminAuth,setRules)
+router.route('/approve-foodandlodge/:id').get(isAuthenticated, adminAuth, approvefoodandlodge)
+router.route('/reject-foodandlodge/:id').get(isAuthenticated, adminAuth, rejectfoodandlodge)
+router.route('/rule').post(isAuthenticated, adminAuth, setRules)
 router.route('/logout').get(logout)
 
 export default router
